276093b6cd7f2d270c0eee9f63efafd34cbce43d
daicav <daicav@fea2a17a-8d1b-11de-b41a-53116b542eef>
2009-09-29 01:34:26 +0000

    En la libreria de sensores se agrega una estructura formada por una muestra y el tiempo en que fue muestreada. En el testeo se realiza un buffer circular en el cual se guardan datos de temperatura.

--- test_sensor.C
+++ test_sensor.C
@@ -2,14 +2,26 @@
 #use "RCM43xx.LIB"
 #use Modem_SIMCOM.lib
 
+#define BUFF_TAM 100
+
+sensor termo[BUFF_TAM];
+
 void main()
 {
-		float temp;
-		brdInit();
+      sensor *ptr_termo, *ptr_termo_fin;
+      brdInit(); 
+
+      ptr_termo = termo;
+      ptr_termo_fin = &termo[BUFF_TAM-1];
+
       while(1)
 		{
-      	esperar(1000);
-      	temp = Termistor();
-         printf("Temperature at %.2f C\n", temp);
+      	esperar(100);
+      	ptr_termo->muestra = Termistor();
+         //printf("Temperature at %.2f C\n", ptr_termo->muestra);
+
+         if (ptr_termo > ptr_termo_fin)
+         	ptr_termo = termo;
+         ptr_termo+=1;
       }
 }
