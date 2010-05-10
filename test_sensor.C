5ccc9a79f9dd2d82f7e8906504cfafa6c78bf65c
edgardovaz <edgardovaz@fea2a17a-8d1b-11de-b41a-53116b542eef>
2010-05-10 22:44:36 +0000

    En test_sensor.C se descomentó una línea para que imprima en pantalla el valor del sensor. En test_gprs.c, logra obtener una ip dinámica pero no logra terminar de establecer una conexión TCP, queda en IP STATUS.

--- test_sensor.C
+++ test_sensor.C
@@ -1,6 +1,6 @@
 #use sensores.lib
 #use "RCM43xx.LIB"
-#use Modem_SIMCOM.lib
+#use MODEM_SIMCOM.LIB
 
 #define BUFF_TAM 100
 
@@ -9,16 +9,16 @@ sensor termo[BUFF_TAM];
 void main()
 {
       sensor *ptr_termo, *ptr_termo_fin;
-      brdInit(); 
+      brdInit();
 
       ptr_termo = termo;
       ptr_termo_fin = &termo[BUFF_TAM-1];
 
       while(1)
 		{
-      	esperar(100);
+      	esperar(1000);
       	ptr_termo->muestra = Termistor();
-         //printf("Temperature at %.2f C\n", ptr_termo->muestra);
+         printf("Temperature at %.2f C\n", ptr_termo->muestra);
 
          if (ptr_termo > ptr_termo_fin)
          	ptr_termo = termo;
