e509616c46615f3c6cac690ccfd6adbb0c123980
edgardovaz <edgardovaz@fea2a17a-8d1b-11de-b41a-53116b542eef>
2010-05-29 00:50:18 +0000

    Se agregan los checksum de todos los mensajes NMEA en la inicialización del gps. Con esto se soluciona el error llamado "Runtime error Index out of bounds" . Se agregó además un mensaje de configuración del puerto serie del gps: "Protocolo de comunicacion NMEA 4800,8,N,1"

--- test_gps.c
+++ test_gps.c
@@ -1,10 +1,9 @@
 #use GPS_ET332.lib
-#use MODEM_SIMCOM.lib
 
 #define tout 50
 #define TAM 200
 
-#define BPS 19200
+
 void main(){
 
 	static char data[TAM];
@@ -21,7 +20,7 @@ void main(){
    	{
    		waitfordone n = cof_serDread(data, sizeof(data), tout);
          data[n]='\0';
-         printf("\n%s\n\n", data);
+         /*printf("\n%s\n\n", data);*/
          ProcesarGPS(data, n);
          n=0;
          sprintf(msj, "Latitud: %s\nLongitud: %s\nHora: %s\nFecha: %s\n\n", latitud, longitud, hora_utc, fecha);
