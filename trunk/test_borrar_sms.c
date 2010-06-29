bd97b418c1df86f4d70e58105a635b3d55a60539
edgardovaz <edgardovaz@fea2a17a-8d1b-11de-b41a-53116b542eef>
2010-06-29 01:52:43 +0000

    La función "Borrar_ALL" se pasó a la librería MODEM_SIMCOM.LIB así como también la función "Listar_SMSs" que imprime en pantalla los mensajes en la memoria del modem. Estas funciones son llamadas desde el archivo test_borrar_sms.c, primero se listan en pantalla todos los mensajes, luego se borran y por último se vuelve a imprimir en pantalla(está vez no hay texto de los mensajes por haber sido borrados).

--- trunk/test_borrar_sms.c
+++ trunk/test_borrar_sms.c
@@ -1,18 +1,10 @@
-#use "RCM43xx.LIB"
 #use MODEM_SIMCOM.LIB
 
-#define BPS      38400
-#define MSJ_TAM  255
-#define MAX_SMS  31
-
-void Borrar_ALL();
+#define BPS	38400
 
 void main()
 {
-		int i;
-		char txt_msj[MSJ_TAM];
-      char command[30];
-      char num_msj[4];
+	int i;
 
   	/*Inicializo el Modem*/
    Inicio_Modem(BPS);
@@ -37,33 +29,10 @@ void main()
 	Config_modo_txt();   						/*Configura el modem en modo texto*/
    printf("Se registro en la red\n\n");
 
+   Listar_SMSs();
    Borrar_ALL();
+   Listar_SMSs();
 
-   /*Imprime los mensajes en memoria del modem*/
-   for(i=1; i<MAX_SMS; i++)
-   {
-   	command[0]='\0';             /*Inicializa el comando*/
-      txt_msj[0]='\0';             /*Inicializa el texto del mensaje a ser leido*/
-      sprintf(num_msj, "%d", i);   /*Conversion de entero a string*/
-      strcat(num_msj, "\r\0");
-      strcpy(command, "AT+CMGR=");
-      strcat(command, num_msj);
-   	Enviar_CMD(command);
-   	Respuesta_Modem(ESPERO_OK, txt_msj, TIEMPO);
-   	printf("Texto del mensaje:\n%s\n", txt_msj);
-   }
    esperar(5000);
    Apagado_Modem();
-}
-
-void Borrar_ALL()
-{
-		int i;
-		char ind_msj[4];
-  		for(i=1; i<MAX_SMS; i++)
-     	{
-        	sprintf(ind_msj, "%d", i);   /*Conversion de entero a string*/
-        	strcat(ind_msj, "\r\0");
-        	Borrar_SMS(ind_msj);         /*Borra el mensaje en la posicion ind_msj*/
-      }
 }
